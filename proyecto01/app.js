dd77a1cda8ae9beea55fd8770983c19bb1b28049
Lia-606 <liapv606@gmail.com>
2025-09-29 23:13:24 -0500

    lunes 29/09

--- proyecto01/app.js
+++ proyecto01/app.js
@@ -1,13 +1,31 @@
-function busquedalineal(arr, elemento) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === elemento) {
-            return i;
-        }
-    }
-    return -1;
-}
+//creacion del servidor:
+const http= require('http')
+const server=http.createServer((request,response)=>{
 
-const arreglo = [10, 5, 3, 8, 2, 6];
-const elementobuscado = 2;
-const indice = busquedalineal(arreglo, elementobuscado);
-console.log(`El elemento ${elementobuscado} se encuentra en el índice ${indice}`);
+    try {
+        if(request.url==='/'&& request.method==='GET'){
+        response.statusCode=200
+        response.end(JSON.stringify({"Message":'ACCEDIENDO AL SERVIDOR '}))
+    }
+    else if(request.url=='/home'&& request.method=='GET'){
+        response.statusCode=300
+        response.end(JSON.stringify({"Message":'ACCEDIENDO A HOME '}))
+    }else{
+        response.statusCode=404
+        response.end(JSON.stringify({"Message":'NO ENCONTRADO '}))
+    }
+        
+    } catch (error) {
+        response.statusCode=500
+        response.end(JSON.stringify({"Message":'Ha ocurrido un error en el servidor '}))
+    }
+
+
+
+})
+
+const port=5000
+const host='localhost'
+server.listen(port,host,()=>{
+    console.log(`servidor ejecutandose en http://${host}:${port}`)
+})
